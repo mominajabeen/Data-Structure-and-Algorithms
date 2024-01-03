87ece649d85615ea458caa7995c81f0d755293a8
mominajabeen <momina.jabeen8@gmail.com>
2024-01-03 20:08:05 +0500

    Time: 0 ms (100.00%), Space: 6.5 MB (20.00%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,8 +1,16 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double res = 0;
-        res = pow(x, n);
+        long exponent = abs(n);
+        double curr = x;
+        double res = 1.0;
+        for(long i = exponent; i>0; i/= 2){
+            if(i%2 == 1){
+                res *= curr;
+            }
+            curr *= curr;
+        }
+        if(n<0) return 1/res;
         return res;
     }
 };
