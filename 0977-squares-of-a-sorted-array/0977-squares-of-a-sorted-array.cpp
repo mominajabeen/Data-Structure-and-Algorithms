ccc4fc9fc80921a1f14440f031108075b2d4e1d2
mominajabeen <momina.jabeen8@gmail.com>
2024-01-23 21:49:04 +0500

    Time: 21 ms (81.14%), Space: 28.3 MB (5.71%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-       for(int i = 0; i < nums.size(); i++){
-           nums[i] = nums[i]*nums[i];
-       }
-        sort(nums.begin(), nums.end());
-        return nums;
+        int l = 0, r = nums.size() - 1;
+        vector<int> res(nums.size());
+        
+        for(int k = nums.size()-1; k >= 0; k--){
+            if(abs(nums[l]) < abs(nums[r])){
+                res[k] = nums[r]*nums[r--];
+            }
+            else{
+                res[k] = nums[l]*nums[l++];
+            }
+        }
+        return res;
     }
 };
+        // vector<int> res(A.size());
+        // int l = 0, r = A.size() - 1;
+        // for (int k = A.size() - 1; k >= 0; k--) {
+        //     if (abs(A[r]) > abs(A[l])) res[k] = A[r] * A[r--];
+        //     // else res[k] = A[l] * A[l++]
