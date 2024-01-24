8c92dcaa4c24c4430f4f8a295f6bcc887e9b2dea
mominajabeen <momina.jabeen8@gmail.com>
2024-01-24 23:36:13 +0500

    Time: 4 ms (52.53%), Space: 7.7 MB (16.01%) - LeetHub

--- 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.cpp
+++ 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.cpp
@@ -3,8 +3,8 @@ public:
     int canBeTypedWords(string text, string brokenLetters) {
         unordered_set<char> set(brokenLetters.begin(), brokenLetters.end());
         
-        int count = 0, res = 0;
-        for(int i = 0; i<= text.size(); i++){
+        int res = 0, count = 0;
+        for(int i = 0; i<= text.size(); ++i){
             if(i == text.size() or text[i] == ' '){
                 res += count == 0;
                 count = 0;
