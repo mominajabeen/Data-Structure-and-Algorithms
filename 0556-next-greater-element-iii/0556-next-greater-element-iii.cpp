1db48b435435b7f93e7fed0ce3f2b789fd2801ea
mominajabeen <momina.jabeen8@gmail.com>
2024-02-08 07:28:34 +0500

    Time: 0 ms (100.00%), Space: 7.1 MB (32.04%) - LeetHub

---
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int nextGreaterElement(int n) {
+        string nums = to_string(n);
+        bool res = nextPermutation(nums);
+        
+        size_t ans = stoll(nums);
+        return (!res or ans > INT_MAX)? -1:ans;
+    }
+    bool nextPermutation(string& nums){
+        int i = nums.size()-1;
+        while(i > 0 && nums[i-1] >= nums[i])    i--;
+        if(i == 0)  return false;
+        
+        int j = nums.size()-1;
+        while(j > 0 && nums[j] <= nums[i-1]) j--;
+        
+        swap(nums[j], nums[i-1]);
+        reverse(nums.begin()+i, nums.end());
+        return true;
+    }
+};
