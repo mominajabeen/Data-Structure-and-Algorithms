8f464d56c80b17f31e9fc1e7e9cab5b34b59ce5e
mominajabeen <momina.jabeen8@gmail.com>
2024-02-15 23:02:54 +0500

    Time: 0 ms (100.00%), Space: 7.1 MB (30.53%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     bool isHappy(int n) {
         int slow = n;
-        int fast = getNext(n);
-        
+        int fast = findSquare(n);
         while(slow != fast && fast != 1){
-            slow = getNext(slow);
-            fast = getNext(getNext(fast));
+            slow = findSquare(slow);
+            fast = findSquare(findSquare(fast));
         }
         if(fast == 1)   return true;
-        else    return false;
+        else return false;
     }
-    int getNext(int n){
-        int sq = 0;
-        int res = 0;
-        while(n){
-            int digit = n%10;
-            sq = digit * digit;
-            res += sq;
-            n /= 10;
+    private:
+    int findSquare(int number){
+        int res= 0;
+        int square = 0;
+        while(number){
+            int digit = number%10;
+            number /= 10;
+            square = (digit*digit);
+            res += square;
         }
         return res;
     }
