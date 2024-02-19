a5e01b5668a7fde461f37179760b69efa1630e05
mominajabeen <momina.jabeen8@gmail.com>
2024-02-19 23:21:36 +0500

    Time: 17 ms (32.28%), Space: 8.9 MB (34.85%) - LeetHub

---
+++ 0457-circular-array-loop/0457-circular-array-loop.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool circularArrayLoop(vector<int>& nums) {
+        
+        for(int i = 0; i<nums.size(); i++){
+            bool isForward = nums[i]>=0;
+            int slow = i, fast = i;
+            
+            do{
+                slow = findNextInd(nums, isForward, slow);
+                fast = findNextInd(nums, isForward, fast);
+                
+                if(fast != -1)  fast = findNextInd(nums, isForward, fast);
+                
+            }while(slow != -1 && fast != -1 && fast != slow);
+                            
+            if(slow != -1 && slow == fast)  return true;
+        }
+        return false;
+    }
+    private:
+    int findNextInd(vector<int>& nums, bool isForward, int currentIndx){
+        bool direction = nums[currentIndx] >= 0;
+        if(isForward != direction)  return -1;
+        
+        int nextIndx = ((currentIndx + nums[currentIndx])+nums.size())%nums.size();
+        if(nextIndx == currentIndx)     return -1;
+        
+        return nextIndx;
+    }
+};
