ff41e25cded23d9de81864a520fcb5bd9057330c
mominajabeen <momina.jabeen8@gmail.com>
2024-02-20 17:48:33 +0500

    Time: 24 ms (73.01%), Space: 22.8 MB (18.88%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if(intervals.size()<2)  return intervals;
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> res;
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        for(int i = 1; i < intervals.size(); i++){
+            if(intervals[i][0]>end){
+                res.push_back({start, end});
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
+            if(intervals[i][0] <= end){
+                end = max(intervals[i][1], end);
+            }
+            if(i == intervals.size()-1){
+                // start = intervals[i][0];
+                // end = intervals[i][1];
+                res.push_back({start, end});
+            }
+        }
+        return res;
+    }
+};
