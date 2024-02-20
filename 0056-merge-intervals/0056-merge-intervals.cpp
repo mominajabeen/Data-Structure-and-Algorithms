c776cc4af471f465c6e0e7339e55bbff2b016f40
mominajabeen <momina.jabeen8@gmail.com>
2024-02-20 17:55:17 +0500

    Time: 11 ms (99.83%), Space: 22.7 MB (37.89%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -7,16 +7,9 @@ public:
         int start = intervals[0][0];
         int end = intervals[0][1];
         for(int i = 1; i < intervals.size(); i++){
-//             if(intervals[i][0]>end){
-//                 res.push_back({start, end});
-
-//             }
             if(intervals[i][0] <= end){
                 end = max(intervals[i][1], end);
             }
-            // if(i == intervals.size()-1){
-                // start = intervals[i][0];
-                // end = intervals[i][1];
             else{
                 res.push_back({start, end});
                 start = intervals[i][0];
