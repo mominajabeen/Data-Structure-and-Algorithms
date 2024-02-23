56577afceddbe5858e843d493a438cf2210bb2f5
mominajabeen <momina.jabeen8@gmail.com>
2024-02-23 21:56:47 +0500

    Time: 2 ms (86.86%), Space: 11.7 MB (9.30%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -13,8 +13,9 @@ public:
     ListNode* reverseList(ListNode* head) {
         ListNode* prev = nullptr;
         ListNode* curr = head;
+        ListNode* temp = nullptr;
         while(curr){
-            ListNode* temp = curr->next;
+            temp = curr->next;
             curr->next = prev;
             prev = curr;
             curr = temp;
