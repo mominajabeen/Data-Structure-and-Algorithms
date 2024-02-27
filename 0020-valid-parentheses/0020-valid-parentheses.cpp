2413c9d97d906ea71121309e4e83f8d3be9ee0e5
mominajabeen <momina.jabeen8@gmail.com>
2024-02-27 22:18:14 +0500

    Time: 3 ms (43.09%), Space: 8 MB (11.79%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     bool isValid(string s) {
-        
         stack<char> stk;
-        for(auto ch:s){
-            if(ch == '(' or ch == '{' or ch == '[')     stk.push(ch);
-            else{
-                if(stk.empty() or (ch == ')' && stk.top() != '(') or (ch == '}' && stk.top() != '{') or (ch == ']' && stk.top() != '[')){
-                    return false;
-                }
-                else    stk.pop();
+        for(char ch:s){
+            if(ch == '(')   stk.push(')');
+            else if(ch == '{')   stk.push('}');
+            else if(ch == '[')   stk.push(']');
+            else if(stk.empty() or stk.top() != ch){
+                return false;
             }
+            else
+            stk.pop();
         }
         return stk.empty();
     }
