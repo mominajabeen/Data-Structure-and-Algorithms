3ee910009d20df58ad9396e2be3647ec87a2b1c3
mominajabeen <momina.jabeen8@gmail.com>
2024-04-16 18:34:28 +0500

    Time: 21 ms (31.19%), Space: 11.1 MB (61.53%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        unordered_map<char, int> map_magazine;
+        unordered_map<char, int> map;
         for(char c:magazine){
-            map_magazine[c]++;
+            map[c]++;
         }
-        unordered_map<char, int> map_ransomNote;
         for(char c:ransomNote){
-            map_ransomNote[c]++;
-        }
-        for(int i = 0; i<map_ransomNote.size(); i++){
-            if(map_ransomNote[i] > map_magazine[i])     return false;
+            if(!map[c]){
+              return false;  
+            }
+            else{
+                map[c]--;
+            }
         }
         return true;
     }
